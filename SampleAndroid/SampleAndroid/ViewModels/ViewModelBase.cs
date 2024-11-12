cefa5b70cdfb7d1dd8739ced65d0420934046c53
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-11-12 22:02:23 +0530

    Add project files.

---
+++ SampleAndroid/SampleAndroid/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SampleAndroid.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ SampleAndroid/SampleAndroid/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SampleAndroid.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
